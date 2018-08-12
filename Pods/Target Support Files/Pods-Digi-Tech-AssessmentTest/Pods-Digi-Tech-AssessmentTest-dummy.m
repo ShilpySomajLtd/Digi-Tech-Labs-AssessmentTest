4624078663b47494165f1fd6f30ef43b5eb8fdad
Ismail Hossain <ihossain.advance@gmail.com>
2018-08-12 09:59:22 +0600

    Commit

---
+++ Pods/Target Support Files/Pods-Digi-Tech-AssessmentTest/Pods-Digi-Tech-AssessmentTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Digi_Tech_AssessmentTest : NSObject
+@end
+@implementation PodsDummy_Pods_Digi_Tech_AssessmentTest
+@end
---
+++ Pods/Target Support Files/Pods-Digi-Tech-AssessmentTest/Pods-Digi-Tech-AssessmentTest-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Digi_Tech_AssessmentTestVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Digi_Tech_AssessmentTestVersionString[];
+
